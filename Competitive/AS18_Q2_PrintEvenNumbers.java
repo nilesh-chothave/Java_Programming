1a370baf4f3e7b5e4b8fde44f8e5e431a211d152
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-06 18:38:24 +0530

    Added new java Program

---
+++ Competitive/AS18_Q2_PrintEvenNumbers.java
@@ -0,0 +1,28 @@
+/*
+    2. Write a program to print all even numbers up to N.
+*/
+
+class Logic
+{
+    void PrintEvenNumbers(int n)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= n; iCnt++)
+        {
+            if(iCnt % 2 == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+    }
+}
+
+class AS18_Q2_PrintEvenNumbers 
+{
+    public static void main(String A[])
+    {
+        Logic obj = new Logic();
+        obj.PrintEvenNumbers(20);
+    }
+}
