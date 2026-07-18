ff28049bafe09327852fc5496644c7f23b592e7e
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-18 18:14:39 +0530

    .git

---
+++ Practice/program38.java
@@ -0,0 +1,13 @@
+class Program38
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 10; iCnt++)
+        {
+            System.out.printf("%d \n",iCnt);
+        }
+
+    }
+}
