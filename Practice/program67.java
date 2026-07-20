fbf017cf777fe922f8140736e736717d728e2f40
nilesh-chothave <nileshchothave2003@gmail.com>
2026-07-20 23:56:31 +0530

    Java Programming Practice Files Uploaded.

---
+++ Practice/program67.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean CheckPrime(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= (iNo / 2) ; iCnt++)
+        {
+            if((iNo % iCnt == 0))
+            {
+                return false;       // Bad programming
+            }
+            
+        }
+        return true;
+    }
+
+}
+
+class Program66
+{
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       int iValue = 0;
+       boolean bRet = false;
+
+       System.out.println("Enter number : ");
+       iValue = sobj.nextInt();
+
+      NumberX nobj = new NumberX();
+
+      bRet = nobj.CheckPrime(iValue);
+
+      if(bRet == true)
+      {
+        System.out.println("It is Prime");
+      }
+      else
+      {
+        System.out.println("It is no Prime");
+      }
+    }
+}
+
+// Time Complexity : O(N/2)
+// Where N >= 0
