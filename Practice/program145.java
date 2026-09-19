68075f1b9daf24f127b2e84e6ff46a13e23781c4
nilesh-chothave <nileshchothave2003@gmail.com>
2026-09-19 23:56:27 +0530

    Java Programming Practice Files Uploaded.

---
+++ Practice/program145.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class program145
+{
+    public static void Update(int Arr[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            Arr[iCnt]++;
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iCnt = 0;
+
+        System.out.println("Enter the number of elements : ");
+        int iLength = sobj.nextInt();
+
+        int Brr[] = new int[iLength];
+
+        System.out.println("Enter the elements : ");
+        for(iCnt = 0; iCnt < Brr.length; iCnt++)
+        {
+            Brr[iCnt] = sobj.nextInt();
+        }
+
+        Update(Brr);
+
+         System.out.println("Array elements after function call : ");
+        for(iCnt = 0; iCnt < Brr.length; iCnt++)
+        {
+            System.out.println(Brr[iCnt]);
+        }
+
+
+        
+    }
+}
